8c8d3a67a92bc27691a94cfc570832683d38e45c
Aayush Chourasiya <aayushchourasiya@hotmail.com>
2021-01-13 11:16:04 +0530

    Added my files

---
+++ www/js/index.js
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+   navigator.geolocation.getCurrentPosition(onSuccess, onError);
+   function onSuccess(position){
+    document.getElementById("geolocation").innerHTML = 
+              'Latitude: '          + position.coords.latitude          + '<br>' +
+              'Longitude: '         + position.coords.longitude         + '<br>' +
+              'Altitude: '          + position.coords.altitude          + '<br>' +
+              'Accuracy: '          + position.coords.accuracy          + '<br>' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br>' +
+              'Heading: '           + position.coords.heading           + '<br>' +
+              'Speed: '             + position.coords.speed             + '<br>' +
+              'Timestamp: '         + position.timestamp                + '<br>';
+   }
+   function onError(error){
+             alert('code: '    + error.code    + '\n' + 
+             'message: ' + error.message + '\n');
+   }
+
+   var options = {frequency :3000}
+   navigator.geolocation.watchPosition(onSuccessWatch, onErrorWatch, options);
+   function onSuccessWatch(watching){
+       document.getElementById("geolocationWatching").innerHTML = 
+              'Latitude: '          + watching.coords.latitude          + '<br>' +
+              'Longitude: '         + watching.coords.longitude         + '<br>' +
+              'Altitude: '          + watching.coords.altitude          + '<br>' +
+              'Accuracy: '          + watching.coords.accuracy          + '<br>' +
+              'Altitude Accuracy: ' + watching.coords.altitudeAccuracy  + '<br>' +
+              'Heading: '           + watching.coords.heading           + '<br>' +
+              'Speed: '             + watching.coords.speed             + '<br>' +
+              'Timestamp: '         + watching.timestamp                + '<br>';
+   }
+   function onErrorWatch(errorMessage){
+        alert('code: '    + errorMessage.code    + '\n' + 
+        'message: ' + errorMessage.message + '\n');
+    }
+}
+
